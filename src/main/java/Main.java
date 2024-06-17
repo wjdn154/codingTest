5c17c8b11efd43920cc51a68bf2d67ed778d47ac
wjdn154 <wjdn154@gmail.com>
2024-06-18 00:21:46 +0900

    [add]2024/06/17 p1181 -java
    https://www.acmicpc.net/group/workbook/view/21059/70317

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,50 @@
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("Hello, !");
+        int N = Integer.parseInt(br.readLine());
+        String list[] = new String[N];
+
+        for(int i=0; i<N; i++)
+            list[i] = br.readLine();
+
+        System.out.println(N);
+        System.out.println(Arrays.toString(list));
+
+        for(int i=0; i<N; i++)
+        {
+            for(int j=i+1; j<N; j++)
+            {
+                if(list[i].length()==list[j].length()) {
+                    if(list[i].compareTo(list[j])>0) {
+                        String tmp = list[j];
+                        list[j]=list[i];
+                        list[i]=tmp;
+                    }
+                }
+                else {
+                    if(list[i].length()>list[j].length()) {
+                        String tmp = list[j];
+                        list[j]=list[i];
+                        list[i]=tmp;
+                    }
+                }
+            }
+        }
+        for(int i=0; i<N; i++) {
+            if(i!=0) {
+                if(list[i].compareTo(list[i-1])!=0)
+                    System.out.println(list[i]);
+            }
+            else
+                System.out.println(list[i]);
+        }
     }
 }
---
+++ src/main/java/repository/p1000.java
@@ -0,0 +1,18 @@
+package repository;
+
+import java.util.Scanner;
+
+public class p1000 {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+
+        System.out.println(A+B);
+
+        in.close();
+    }
+}
+
