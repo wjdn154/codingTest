96be5b9b964afdbec56d57250807589b231b8b91
wjdn154 <wjdn154@gmail.com>
2024-06-19 00:26:05 +0900

    [add]2024/06/17 p1152 -java

--- src/main/java/repository/p1152.java
+++ src/main/java/repository/p1152.java
@@ -1,9 +1,11 @@
+package repository;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class p1152 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine().trim();
