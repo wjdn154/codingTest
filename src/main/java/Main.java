364620af0fe847d3e2715c094c13a96521a87ab9
wjdn154 <wjdn154@gmail.com>
2024-06-20 23:39:51 +0900

    [add]2024/06/17 p2438 -java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        br.close();
 
-        for(int i = 1; i <= N; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
     }
 }
