982b9793ecdea71223daa02a6d681dd7a886f1b1
wjdn154 <wjdn154@gmail.com>
2024-06-20 23:18:30 +0900

    [add]2024/06/17 p2557 -java

--- src/main/java/repository/p2557.java
+++ src/main/java/repository/p2557.java
@@ -1,13 +1,12 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+package repository;
 
-public class Main {
+import java.io.IOException;
+
+public class p2557 {
     public static void main(String[] args) throws IOException {
 
         System.out.println("Hello World!");
 
     }
+
 }
