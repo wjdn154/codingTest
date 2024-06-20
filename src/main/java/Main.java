8277b75ab33cb9985116eece9bb02fecbe71a5bc
wjdn154 <wjdn154@gmail.com>
2024-06-20 23:48:33 +0900

    [add]2024/06/17 p9498 -java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,14 +5,19 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        br.close();
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+        int N = Integer.parseInt(br.readLine());
+
+        if(N >= 90) {
+            System.out.println("A");
+        }else if(N >= 80) {
+            System.out.println("B");
+        }else if(N >= 70) {
+            System.out.println("C");
+        }else if(N >= 60) {
+            System.out.println("D");
+        }else {
+            System.out.println("F");
         }
     }
 }
