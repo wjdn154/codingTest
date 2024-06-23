3e8e04f88c56d9f9f57e00e42123da3b3127c791
wjdn154 <wjdn154@gmail.com>
2024-06-24 01:13:31 +0900

    [add]2024/06/24 p1475 -java

--- src/main/java/repository/p1475.java
+++ src/main/java/repository/p1475.java
@@ -1,10 +1,10 @@
+package repository;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
-public class Main {
+public class p1475 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
