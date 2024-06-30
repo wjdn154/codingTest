40bff44d1e4eca4be92ee8abd70d7fd4d219b8f9
wjdn154 <wjdn154@gmail.com>
2024-06-30 09:19:47 +0900

    [add]2024/06/30 p1406 -java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
     }
 }
 
